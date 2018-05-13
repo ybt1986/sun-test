863bc750986e76869f78e07de31f51da5530a308
suncht <sss>
2018-05-13 14:08:08 +0800

    增加端口和context-path对访问的影响

--- springboot.tomcat.test/src/main/java/com/example/demo/DemoApplication.java
+++ springboot.tomcat.test/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,37 @@
 package com.example.demo;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	// 用于处理编码问题  
+		@Bean
+		public Filter characterEncodingFilter() {
+			CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+			characterEncodingFilter.setEncoding("UTF-8");
+			characterEncodingFilter.setForceEncoding(true);
+			return characterEncodingFilter;
+		}
+		
+		/**
+		 * mvn打包后，需要指定内嵌tomcat的服务，否则会“找不到EmbeddedServletContainerFactory”报错
+		 * 也可以使用jetty服务
+		 * @return
+		 */
+		@Bean
+		public EmbeddedServletContainerFactory servletContainer() {
+			TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+			return factory;
+		}
+		
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
