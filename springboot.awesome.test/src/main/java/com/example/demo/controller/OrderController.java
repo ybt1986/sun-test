342d90774a25b53f172f8585c7475711d917fcff
suncht <sss>
2018-11-14 22:41:49 +0800

    aaaaaa

--- springboot.awesome.test/src/main/java/com/example/demo/controller/OrderController.java
+++ springboot.awesome.test/src/main/java/com/example/demo/controller/OrderController.java
@@ -5,10 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/order/v1")
 public class OrderController {
-	@RequestMapping({"", "/", "index"})  
-    public String index(Model model){  
-        return "/order";  
-    }  
+	@RequestMapping("/order1.do")
+	public String order1(Model model) {
+		return "/order";
+	}
+
+	@RequestMapping("/order2.do")
+	public String order2(Model model) {
+		return "/order2";
+	}
+
+	@RequestMapping("/simple.do")
+	public String simple(Model model) {
+		return "/simple";
+	}
 }
