3660b5eb92a73914a9bdf2e121b07aa9b3656778
sunchangtan <sunchangtan@jd.com>
2018-11-15 19:01:29 +0800

    kkk

--- springboot.awesome.test/src/main/java/com/example/demo/cmp/resolver/PathMatchingResourceFileResolver.java
+++ springboot.awesome.test/src/main/java/com/example/demo/cmp/resolver/PathMatchingResourceFileResolver.java
@@ -8,27 +8,35 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
+import com.example.demo.utils.CompressorUtils;
+
 public class PathMatchingResourceFileResolver implements ResourceResolver {
 	private ResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
 	
 	@Override
-	public File getFile(String resourceLocation) throws IOException {
+	public InputStream getFile(String resourceLocation) throws IOException {
 		String path = resourceLocation;
+		boolean isMin = true;
 		Resource[] resources = resourceResolver.getResources(path);
 		if(resources == null || resources.length == 0 || !resources[0].exists()) {
 			//尝试使用非min文件
 			if(path.contains(".min.")) {
 				path = path.replace(".min.", ".");
 			}
-			
 			resources = resourceResolver.getResources(path);
+			isMin = false;
 		}
 		
 		if(resources == null || resources.length == 0|| !resources[0].exists()) {
 			return null;
 		}
 		
-		return resources[0].getFile();
+		File file = resources[0].getFile();
+		if(!isMin) {
+			//非压缩文件
+		}
+		
+		CompressorUtils.compressJS(file, out);
 	}
 
 	@Override
--- springboot.awesome.test/src/main/java/com/example/demo/cmp/resolver/ResourceResolver.java
+++ springboot.awesome.test/src/main/java/com/example/demo/cmp/resolver/ResourceResolver.java
@@ -1,11 +1,11 @@
 package com.example.demo.cmp.resolver;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 public interface ResourceResolver {
 
-	File getFile(String resourceLocation) throws IOException;
+	InputStream getFile(String resourceLocation) throws IOException;
 	
 	boolean exist(String resourceLocation);
 
